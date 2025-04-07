034545ea016bea7f02ce277f6364c9f05723b390
albertou <63100245+albertou2311@users.noreply.github.com>
2025-04-07 23:08:26 +0300

    Update guvenlik.py

--- guvenlik.py
+++ guvenlik.py
@@ -1,6 +1,9 @@
 import winsound
 
 def guvenlik_uyarisi():
+    print("[!] Güvenlik uyarısı: Lütfen dikkatli olun, sisteminizin güvenliği önemli!")
+
+def guvenlik_uyarisi():
     print("Kritik güvenlik açığı tespit edildi!")
     winsound.Beep(1000, 1000)  # Sesli uyarı
 
