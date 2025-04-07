554b1faf9ff18cf0217047528d3dcc79da414340
albertou <63100245+albertou2311@users.noreply.github.com>
2025-04-07 17:44:48 +0300

    Update platform_check.py

--- platform_check.py
+++ platform_check.py
@@ -1,14 +1,18 @@
-import platform
 import subprocess
+import platform
+
+print("You are running on:", platform.system())
 
 # Eğer Windows'taysak
 if platform.system() == "Windows":
     print("Windows ortamında çalıştırılıyor...")
-    # Windows'a uygun komutları çalıştırabiliriz
-    subprocess.run(["ipconfig"])  # Windows'da IP bilgisi almak için ipconfig kullanılır.
+    # Windows'a uygun komutları çalıştırıyoruz
+    result = subprocess.run(["ipconfig"], capture_output=True, text=True)
+    print(result.stdout)  # Komutun çıktısını yazdırıyoruz
 
 # Eğer Linux'taysak
 elif platform.system() == "Linux":
     print("Linux ortamında çalıştırılıyor...")
-    # Linux'a uygun komutları çalıştırabiliriz
-    subprocess.run(["ifconfig"])  # Linux'ta ifconfig çalışır
+    # Linux'a uygun komutları çalıştırıyoruz
+    result = subprocess.run(["ifconfig"], capture_output=True, text=True)
+    print(result.stdout)  # Komutun çıktısını yazdırıyoruz
