5b29c671e19dcf2b36c26c4ceeb118eb5d19e4ed
albertou <63100245+albertou2311@users.noreply.github.com>
2025-04-08 20:04:01 +0300

    Create komutlar__init__.py

---
+++ komutlar/komutlar__init__.py
@@ -0,0 +1,23 @@
+# komutlar/__init__.py dosyasına yapıştır!
+
+from .komutlar import (
+    komut_tanima,
+    apache_kapat,
+    sistem_temizligi,
+    guvenlik_taramasi,
+    sistemi_guncelle,
+    sistemi_yeniden_baslat,
+    disk_temizligi,
+    sistem_saglik_kontrol,
+    gereksiz_servisleri_kapat,
+    nmap_tarama,
+    ssh_baglantisi_ve_sniper_tarama,
+    armitage_kullan,
+    ip_degistir,
+    mac_degistir,
+    guvenlik_duvari_kontrol,
+    vmware_baslat,
+    kali_linux_baslat,
+    sistemi_kapat,
+    rapor_yaz
+)
