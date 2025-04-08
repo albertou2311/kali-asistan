b0c350598195d14a262483741d2790d7c2702a53
albertou <63100245+albertou2311@users.noreply.github.com>
2025-04-09 00:52:27 +0300

    Update komut_tanima.py

--- komutlar/komut_tanima.py
+++ komutlar/komut_tanima.py
@@ -1,1 +1,12 @@
 from komutlar.komut_tanima import fonksiyon_adı
+
+
+# komutlar/komut_tanima.py
+
+def komut_tanima(komut):
+    # Komut tanıma işlemi burada yapılacak
+    print(f"Komut alındı: {komut}")
+    return komut
+
+
+
