8b1d48da3e05291a0ff00501c16894415aeee1a8
albertou <63100245+albertou2311@users.noreply.github.com>
2025-04-08 22:28:02 +0300

    Update __init__.py

--- komutlar/__init__.py
+++ komutlar/__init__.py
@@ -1,1 +1,23 @@
-touch komutlar/__init__.py
+# komutlar/__init__.py dosyasına yapıştır!
+
+from .komutlar import (
+    komut_tanima,
+    apache_kapat,
+    sistem_temizligi,
+    guvenlik_taramasi,
+    sistemi_guncelle,
+    sistemi_yeniden_baslat,
+    disk_temizligi,
+    sistem_saglik_kontrol,
+    gereksiz_servisleri_kapat,
+    nmap_tarama,
+    ssh_baglantisi_ve_sniper_tarama,
+    armitage_kullan,
+    ip_degistir,
+    mac_degistir,
+    guvenlik_duvari_kontrol,
+    vmware_baslat,
+    kali_linux_baslat,
+    sistemi_kapat,
+    rapor_yaz
+)
