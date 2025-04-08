b5069f3f7efc47a40dc51cbc80fd9d7131cfbe8a
albertou <63100245+albertou2311@users.noreply.github.com>
2025-04-08 23:14:58 +0300

    Update __init__.py

---
+++ komutlar/__init__.py
@@ -0,0 +1,21 @@
+# komutlar/__init__.py
+
+from .komutlar import (
+    apache_kapat,
+    sistemi_guncelle,
+    sistem_temizligi,
+    guvenlik_taramasi,
+    sistemi_yeniden_baslat,
+    disk_temizligi,
+    sistem_saglik_kontrol,
+    gereksiz_servisleri_kapat,
+    nmap_tarama,
+    ssh_baglantisi_ve_sniper_tarama,
+    armitage_kullan,
+    ip_degistir,
+    mac_degistir,
+    guvenlik_duvari_kontrol,
+    vmware_baslat,
+    kali_linux_baslat,
+    sistemi_kapat
+)
