b67498cbbfb0db9eb5415e7a0e1113fdcfcbc184
albertou <63100245+albertou2311@users.noreply.github.com>
2025-04-09 02:24:07 +0300

    Update loglayici.py

--- loglayici.py
+++ loglayici.py
@@ -1,24 +1,12 @@
 # loglayici.py
 import logging
-from datetime import datetime
-import os
-
-def log_konfiguru_et():
-    tarih = datetime.now().strftime("%d-%m-%Y")
-    log_dosyasi = f"logs/log_{tarih}.txt"
-    os.makedirs("logs", exist_ok=True)
-    
-    logging.basicConfig(
-        filename=log_dosyasi,
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
 
 def logla(mesaj, seviye="info"):
-    log_konfiguru_et()
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
     if seviye == "info":
-        logging.info(mesaj)
+        logger.info(mesaj)
     elif seviye == "warning":
-        logging.warning(mesaj)
+        logger.warning(mesaj)
     elif seviye == "error":
-        logging.error(mesaj)
+        logger.error(mesaj)
